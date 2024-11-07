e79da5d37e0290c74e3c10b6880a623dbb34a867
홍다영 <dyhong@aimmed.com>
2024-11-07 17:59:05 +0900

    feat: alias 설정

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
           '@': './src',
+          '@screens': './src/screens',
         },
       },
     ],
